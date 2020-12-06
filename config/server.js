cbdf208db8cda9b0a5d1cd5a42fae590396ab59e
SophieTaule <49538115+SophieTaule@users.noreply.github.com>
2020-12-06 10:40:07 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd5c5487088c2204e232370c879f8b5e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5e0938fb-1951-44b3-8662-3f0b0005a645'
+};
